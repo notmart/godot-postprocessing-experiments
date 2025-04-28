c9d36513ee95110f69b8288bfd9538eee19e7325
Marco Martin <notmart@gmail.com>
2025-04-28 21:03:15 +0200

    try the sphere based approach

--- proto_controller.gd
+++ proto_controller.gd
@@ -80,6 +80,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	# If freeflying, handle freefly and nothing else
+	$MeshInstance3D.rotation.y = -rotation.y
 	if can_freefly and freeflying:
 		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
 		var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
